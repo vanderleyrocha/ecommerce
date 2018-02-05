5ade5e55f19a549c8a7ddd537b5577e4123b2c37
Escola João Aguiar <vanderley@outlook.com>
2018-02-05 00:10:18 -0500

    Admin Categorias

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -164,8 +165,61 @@ $app->post('/admin/forgot/reset', function()
 
 	$page->setTpl("forgot-reset-success");
 
-	//header("Location: /admin/forgot/sent");
-	//exit;
+});
+
+$app->get('/admin/categories', function() 
+{ 
+	$categories = Category::listAll(); 
+	$page = new PageAdmin();
+	$page->setTpl("categories", ["categories"=>$categories]);
+});
+
+$app->get('/admin/categories/create', function() 
+{
+	$page = new PageAdmin();
+	$page->setTpl("categories-create");
+});
+
+$app->post('/admin/categories/create', function() 
+{
+	$category = new Category();
+	$category->setData($_POST);
+	$category->save();
+	header("Location: /admin/categories");
+	exit;
+});
+
+$app->get('/admin/categories/:idcategory/delete', function($idcategory) 
+{   
+	User::verifyLogin();
+
+	$category = new Category();
+	$category->get((int)$idcategory);
+	var_dump($category);
+	$category->delete();
+
+	header("Location: /admin/categories");
+	exit;		
+});
+
+$app->get('/admin/categories/:idcategory', function($idcategory) 
+{   
+	User::verifyLogin();	
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$page = new PageAdmin();
+	$page->setTpl("categories-update", array("category"=>$category->getValues()));	
+});
+
+$app->post('/admin/categories/:idcategory', function($idcategory) 
+{   
+	User::verifyLogin();		
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$category->setData($_POST);
+	$category->save();
+	header("Location: /admin/categories");
+	exit;		
 });
 
 $app->run();
